ae1b9f48f32dc588b077927f3f7c46fcb036504b
UngoKef <ungokeflavik@gmail.com>
2023-06-01 09:44:59 +0000

    bua til matsedill

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,15 +9,7 @@ const body = (() => {
   var link = window.location.pathname;
 
   function init(header_container, sectionRight) {
-    // let hc = header_container;
-    // console.log(hc);
-    // console.log(hc.children);
-    // console.log(hc.children[1].children[0]);
-    // console.log('hello, mam, how are you');
-    // console.log(sectionRight);
-
     checkLink(sectionRight);
-
   }
 
   /*************************
@@ -28,20 +20,23 @@ const body = (() => {
    *
    ************************/
   function checkLink(sr) {
-    if(link === '/' || link === '/index.html' || link === '/ungo.github.io/') {
-      sr[0].style.color = 'red';
+    if(link === '/' || link === '/index.html' || link === '/ungo.github.io/' || link === '/ungo.is/') {
+      sr[0].style.color = '#ff726f';
     }
-    else if(link === '/matsedill_ungo.html' || link === '/ungo.github.io/matsedill_ungo.html') {
-      sr[1].style.color = 'red';
+    else if(link === '/matsedill_ungo.html' || link === '/ungo.github.io/matsedill_ungo.html' || link === '/ungo.is/matsedill_ungo.html') {
+      sr[1].style.color = '#ff726f';
     }
-    else if(link === '/tilbod_ungo.html' || link === '/ungo.github.io/tilbod_ungo.html') {
-      sr[2].style.color = 'red';
+    else if(link === '/is_ungo.html' || link === '/ungo.github.io/is_ungo.html' || link === '/ungo.is/is_ungo.html') {
+      sr[2].style.color = '#ff726f';
     }
-    else if(link === '/um_ungo.html' || link === '/ungo.github.io/um_ungo.html') {
-      sr[3].style.color = 'red';
+    else if(link === '/tilbod_ungo.html' || link === '/ungo.github.io/tilbod_ungo.html' || link === '/ungo.is/tilbod_ungo.html') {
+      sr[3].style.color = '#ff726f';
     }
-    else if(link === '/english_menu_ungo.html' || link === '/ungo.github.io/english_menu_ungo.html') {
-      sr[4].style.color = 'red';
+    else if(link === '/um_ungo.html' || link === '/ungo.github.io/um_ungo.html' || link === '/ungo.is/um_ungo.html') {
+      sr[4].style.color = '#ff726f';
+    }
+    else if(link === '/english_menu_ungo.html' || link === '/ungo.github.io/english_menu_ungo.html' || link === '/ungo.is/english_menu_ungo.html') {
+      sr[5].style.color = '#ff726f';
     }
   }
 
